31c873e625dfebccdf934d86920088161788a833
duxiaodong <duxiaodong@darlin.me>
2016-05-31 12:37:56 +0800

    callback

--- app/third/qq.py
+++ app/third/qq.py
@@ -76,11 +76,11 @@ def callback(obj):
 
 
 def get_user(request, url_params, access_token):
-    print 'get_user'
+    print 'get_user', url_params
     # url_params = 'callback( {"client_id":"101322546","openid":"442F5423D8457D1C8DCF2B5D01023B25"} );'
-    url_params = url_params.strip(';')
-    print url_params
-    ret = eval(url_params)
+    new_url_params = url_params.strip(';')
+    print new_url_params
+    ret = eval(new_url_params)
     print ret
     url = 'https://graph.qq.com/user/get_user_info'
     data = {
