616f63d6000481afa4272dcb4937c1264fb60dff
duxiaodong <duxiaodong@darlin.me>
2016-05-31 10:59:35 +0800

    lalala

--- app/third/qq.py
+++ app/third/qq.py
@@ -52,16 +52,12 @@ def get_access_token(request):
         }
         data = urllib.urlencode(data)
         complete_url = url + '?' + data
-        print complete_url
         ret = requests.get(complete_url)
-        print ret.text, 22222
         access_token = urlparse.parse_qs(ret.text)['access_token'][0]
-        print access_token, 33333
         return get_user_openid(request, access_token)
 
 
 def get_user_openid(request, access_token):
-    print access_token, 4444
     url = 'https://graph.qq.com/oauth2.0/me'
     data = {
         'access_token': access_token,
@@ -83,9 +79,8 @@ def callback(obj):
 
 
 def get_user(request, url_params, access_token):
-    url_params()
-    print 'get_user', url_params
     ret = eval(url_params)  # callback( {"client_id":"101322546","openid":"442F5423D8457D1C8DCF2B5D01023B25"} );
+    print ret
     url = 'https://graph.qq.com/user/get_user_info'
     data = {
         'access_token': access_token,
