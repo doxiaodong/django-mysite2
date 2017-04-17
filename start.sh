1ce924e1ebb00bb424be9fadd2c9f5c8529d5eb0
duxiaodong <duxiaodong@darlin.me>
2017-04-17 14:29:42 +0800

    update

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 # source env/bin/activate
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:9999
