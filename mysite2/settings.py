80924577a3a51b1f2d9b7dfd34eb12f8172aeb14
duxiaodong <duxiaodong@darlin.me>
2018-09-04 19:19:44 +0800

    charset

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -127,6 +127,15 @@ else:
             'CHARSET': 'utf8mb4',
             'COLLATION': 'utf8mb4_unicode_ci',
             'PORT': MYSQL.get('PORT'),
+            'OPTIONS': {
+                # Tell MySQLdb to connect with 'utf8mb4' character set
+                'charset': 'utf8mb4',
+            },
+            # Tell Django to build the test database with the 'utf8mb4' character set
+            'TEST': {
+                'CHARSET': 'utf8mb4',
+                'COLLATION': 'utf8mb4_unicode_ci',
+            },
         }
     }
 
